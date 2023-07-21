4b1fe780e391a19fab4b87e29cb233b3f4c8f1c1
Igna Luna <ignaluna98pb@gmail.com>
2023-07-20 21:47:21 -0300

    Estadisticas Animadas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,16 +20,16 @@ const Navbar = () => {
   return (
     <nav
       className={`navbar flex items-center justify-between px-12 fixed top-0 left-0 w-full z-10 bg-white shadow 
-      transition-all ${
+      transition-all duration-300 ${
         scrolled ? "h-12" : "h-[20vh]"
       }`}
     >
-      <div className={`logo transition-all duration-700  ${scrolled ? "scale-100" : ""}`} id="logo">
+      <div className={`logo transition-all duration-700  ${scrolled ? "scale-100" : "scale-150"}`} id="logo">
         <Image
           src="https://res.cloudinary.com/dvh8hozns/image/upload/v1688157403/Anunciar/qpi5qareodaxmmehor3u.png"
           alt="Logo"
-          height={scrolled ? 100 : 150} // Cambia el tamaño de acuerdo con el estado de "scrolled"
-          width={scrolled ? 100 : 150}  // Cambia el tamaño de acuerdo con el estado de "scrolled"
+          height={100} // Cambia el tamaño de acuerdo con el estado de "scrolled"
+          width={100}  // Cambia el tamaño de acuerdo con el estado de "scrolled"
         />
       </div>
     </nav>
