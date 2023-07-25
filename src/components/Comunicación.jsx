3882cfaa9ae7d9d026e960ac4f3fddbb09092197
Igna Luna <ignaluna98pb@gmail.com>
2023-07-24 23:08:51 -0300

    24/07 fixing errors

--- src/components/Comunicación.jsx
+++ src/components/Comunicación.jsx
@@ -31,7 +31,7 @@ const Comunicación = () => {
     {/* <img class="xl:w-1/4 lg:w-1/3 md:w-1/2 w-2/3 block mx-auto mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"> */}
     <div class="flex flex-col text-center w-full">
       <h1 class="text-xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably havent heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
     </div>
   </div>
 </section>  
